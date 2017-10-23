9753ec2bfd825dc9bfe1c1012076013ac45b1fb4
Michael Golisch <mgolisch@googlemail.com>
2017-10-23 18:31:52 +0200

    change config option

--- run_hassio.sh
+++ run_hassio.sh
@@ -5,5 +5,5 @@ MQTT_BROKER_URL="$(jq --raw-output '.mqtt_broker' $CONFIG_PATH)"
 MQTT_BROKER_PORT="$(jq --raw-output '.mqtt_port' $CONFIG_PATH)"   
 MQTT_USERNAME="$(jq --raw-output '.mqtt_username' $CONFIG_PATH)"   
 MQTT_PASSWORD="$(jq --raw-output '.mqtt_password' $CONFIG_PATH)"
-export SQLALCHEMY_DATABASE_URI MQTT_BROKER_URL MQTT_BROKER_PORT MQTT_USERNAME MQTT_PASSWORD
+export DATABASE_FILE MQTT_BROKER_URL MQTT_BROKER_PORT MQTT_USERNAME MQTT_PASSWORD
 python run.py   
